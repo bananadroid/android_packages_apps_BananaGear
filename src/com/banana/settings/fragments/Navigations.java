14b69e6045aa1b3e2152e906a157a00d7e8c97ca
bigrushdog <randall.rushing@gmail.com>
2020-12-09 14:43:07 -0500

    Gear: Add navigation bar visibility control [2/2]
    Change-Id: I5eb9f3e452102c62555fb117256abf464ee5c2b5

--- src/com/banana/settings/fragments/Navigations.java
+++ src/com/banana/settings/fragments/Navigations.java
@@ -19,6 +19,10 @@ package com.banana.settings.fragments;
 import android.content.Context;
 import android.os.Bundle;
 import android.provider.SearchIndexableResource;
+import android.content.ContentResolver;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
 import androidx.preference.*;
 
 import com.android.internal.logging.nano.MetricsProto;
@@ -36,11 +40,16 @@ import java.util.List;
 public class Navigations extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener, Indexable {
 
+    private static final String NAVBAR_VISIBILITY = "navbar_visibility";
     private static final String LAYOUT_SETTINGS = "navbar_layout_views";
     private static final String NAVIGATION_BAR_INVERSE = "navbar_inverse_layout";
 
     private Preference mLayoutSettings;
     private SwitchPreference mSwapNavButtons;
+    private SwitchPreference mNavbarVisibility;
+
+    private boolean mIsNavSwitchingMode = false;
+    private Handler mHandler;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -50,14 +59,46 @@ public class Navigations extends SettingsPreferenceFragment implements
 
         mLayoutSettings = (Preference) findPreference(LAYOUT_SETTINGS);
         mSwapNavButtons = (SwitchPreference) findPreference(NAVIGATION_BAR_INVERSE);
+        mNavbarVisibility = (SwitchPreference) findPreference(NAVBAR_VISIBILITY);
 
         if (!bananaUtils.isThemeEnabled("com.android.internal.systemui.navbar.threebutton")) {
             prefScreen.removePreference(mLayoutSettings);
         }
+
+        boolean defaultToNavigationBar = bananaUtils.deviceSupportNavigationBar(getActivity());
+        boolean showing = Settings.System.getInt(getContentResolver(),
+                Settings.System.FORCE_SHOW_NAVBAR,
+                defaultToNavigationBar ? 1 : 0) != 0;
+        updateBarVisibleAndUpdatePrefs(showing);
+
+        mNavbarVisibility.setOnPreferenceChangeListener(this);
+
+        mHandler = new Handler();
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference.equals(mNavbarVisibility)) {
+            if (mIsNavSwitchingMode) {
+                return false;
+            }
+            mIsNavSwitchingMode = true;
+            boolean showing = ((Boolean)newValue);
+            Settings.System.putInt(getContentResolver(), Settings.System.FORCE_SHOW_NAVBAR,
+                    showing ? 1 : 0);
+            updateBarVisibleAndUpdatePrefs(showing);
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mIsNavSwitchingMode = false;
+                }
+            }, 1500);
+            return true;
+        }
         return false;
+    }
+
+    private void updateBarVisibleAndUpdatePrefs(boolean showing) {
+        mNavbarVisibility.setChecked(showing);
     }
 
     @Override
