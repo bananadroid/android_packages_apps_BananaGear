70209aa2d615f24e5c9cdb5f034648d22387c170
Genkzsz11 <Genkzsz11@gmail.com>
2020-12-24 16:29:28 +0100

    Gear: add Gvisual Mod [2/3]
    Change-Id: I753e7e65fff31d3104983aacbf2529453446516d

--- src/com/banana/settings/fragments/Themes.java
+++ src/com/banana/settings/fragments/Themes.java
@@ -16,11 +16,18 @@
 
 package com.banana.settings.fragments;
 
+import static android.os.UserHandle.USER_SYSTEM;
+
+import android.app.Activity;
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.om.IOverlayManager;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.provider.Settings;
 
 import androidx.fragment.app.Fragment;
@@ -28,6 +35,7 @@ import androidx.preference.*;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.banana.ThemesUtils;
 import com.android.internal.util.banana.bananaUtils;
 
 import com.android.settings.R;
@@ -52,6 +60,14 @@ public class Themes extends DashboardFragment implements OnPreferenceChangeListe
 
     public static final String TAG = "Themes";
 
+    private static final String PREF_ROUNDED_CORNER = "rounded_ui";
+    private static final String PREF_SB_HEIGHT = "statusbar_height";
+
+    private IOverlayManager mOverlayManager;
+    private IOverlayManager mOverlayService;
+    private ListPreference mRoundedUi;
+    private ListPreference mSbHeight;
+
     private IntentFilter mIntentFilter;
     private static FontPickerPreferenceController mFontPickerPreference;
 
@@ -68,12 +84,98 @@ public class Themes extends DashboardFragment implements OnPreferenceChangeListe
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mOverlayService = IOverlayManager.Stub
+                .asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
+
+        mRoundedUi = (ListPreference) findPreference(PREF_ROUNDED_CORNER);
+        int roundedValue = getOverlayPosition(ThemesUtils.UI_RADIUS);
+        if (roundedValue != -1) {
+            mRoundedUi.setValue(String.valueOf(roundedValue + 2));
+        } else {
+            mRoundedUi.setValue("1");
+        }
+        mRoundedUi.setSummary(mRoundedUi.getEntry());
+        mRoundedUi.setOnPreferenceChangeListener(this);
+
+        mSbHeight = (ListPreference) findPreference(PREF_SB_HEIGHT);
+        int sbHeightValue = getOverlayPosition(ThemesUtils.STATUSBAR_HEIGHT);
+        if (sbHeightValue != -1) {
+            mSbHeight.setValue(String.valueOf(sbHeightValue + 2));
+        } else {
+            mSbHeight.setValue("1");
+        }
+        mSbHeight.setSummary(mSbHeight.getEntry());
+        mSbHeight.setOnPreferenceChangeListener(this);
+
         mIntentFilter = new IntentFilter();
         mIntentFilter.addAction("com.android.server.ACTION_FONT_CHANGED");
     }
 
+    public void handleOverlays(String packagename, Boolean state, IOverlayManager mOverlayManager) {
+        try {
+            mOverlayService.setEnabled(packagename, state, USER_SYSTEM);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mRoundedUi) {
+            String rounded = (String) newValue;
+            int roundedValue = Integer.parseInt(rounded);
+            mRoundedUi.setValue(String.valueOf(roundedValue));
+            String overlayName = getOverlayName(ThemesUtils.UI_RADIUS);
+            if (overlayName != null) {
+                handleOverlays(overlayName, false, mOverlayManager);
+            }
+            if (roundedValue > 1) {
+                handleOverlays(ThemesUtils.UI_RADIUS[roundedValue -2],
+                        true, mOverlayManager);
+            }
+            mRoundedUi.setSummary(mRoundedUi.getEntry());
+            return true;
+        } else if (preference == mSbHeight) {
+            String sbheight = (String) newValue;
+            int sbheightValue = Integer.parseInt(sbheight);
+            mSbHeight.setValue(String.valueOf(sbheightValue));
+            String overlayName = getOverlayName(ThemesUtils.STATUSBAR_HEIGHT);
+            if (overlayName != null) {
+                handleOverlays(overlayName, false, mOverlayManager);
+            }
+            if (sbheightValue > 1) {
+                handleOverlays(ThemesUtils.STATUSBAR_HEIGHT[sbheightValue -2],
+                        true, mOverlayManager);
+            }
+            mSbHeight.setSummary(mSbHeight.getEntry());
+            return true;
+        }
         return false;
+    }
+
+    private int getOverlayPosition(String[] overlays) {
+        int position = -1;
+        for (int i = 0; i < overlays.length; i++) {
+            String overlay = overlays[i];
+            if (bananaUtils.isThemeEnabled(overlay)) {
+                position = i;
+            }
+        }
+        return position;
+    }
+
+    private String getOverlayName(String[] overlays) {
+        String overlayName = null;
+        for (int i = 0; i < overlays.length; i++) {
+            String overlay = overlays[i];
+            if (bananaUtils.isThemeEnabled(overlay)) {
+                overlayName = overlay;
+            }
+        }
+        return overlayName;
     }
 
     @Override
