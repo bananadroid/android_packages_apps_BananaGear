1ddb7ac824507d8dfbdcd5476fe88f06a29fb045
Pranav Vashi <neobuddy89@gmail.com>
2021-02-06 03:17:44 +0530

    Gear: QS footer icon visibilities [2/2]
    Include Allow to quickly open Running Services from QS panel [1/2]
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I1192bc67b1d7132a1119dddb85a2264ef463d57a

--- src/com/banana/settings/fragments/QuickSettings.java
+++ src/com/banana/settings/fragments/QuickSettings.java
@@ -78,6 +78,16 @@ public class QuickSettings extends SettingsPreferenceFragment implements
                 Settings.System.SHOW_QS_DATE, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.QS_FOOTER_DATA_USAGE, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.QS_FOOTER_SHOW_SETTINGS, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.QS_FOOTER_SHOW_SERVICES, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.QS_FOOTER_SHOW_EDIT, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.QS_FOOTER_SHOW_USER, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.QS_FOOTER_SHOW_POWER_MENU, 1, UserHandle.USER_CURRENT);
     }
 
     @Override
