e2eb7f463cf12e178a9acacdf68a036a77f0b600
Anushek Prasal <anushekprasal@gmail.com>
2021-05-31 23:47:02 +0530

    Gear: Introduce automated Sleep Mode [3/3]
    Change-Id: I569576092f60e24220f15d11671586de8d40f626
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- src/com/banana/settings/fragments/Misc.java
+++ src/com/banana/settings/fragments/Misc.java
@@ -28,6 +28,7 @@ import android.os.UserHandle;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 import android.text.TextUtils;
+import android.text.format.DateFormat;
 import androidx.preference.*;
 
 import com.android.internal.logging.nano.MetricsProto;
@@ -44,6 +45,8 @@ import com.bananadroid.support.preferences.SystemSettingMasterSwitchPreference;
 import com.banana.settings.preferences.AppMultiSelectListPreference;
 import com.banana.settings.preferences.ScrollAppsViewPreference;
 
+import java.time.format.DateTimeFormatter;
+import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -65,6 +68,7 @@ public class Misc extends SettingsPreferenceFragment implements
     private static final String AMBIENT_NOTIFICATION_LIGHT = "pulse_ambient_light";
     private static final String KEY_CUTOUT_CATEGORY = "cutout_category";
 
+    private Preference mSleepMode;
     private GlobalSettingMasterSwitchPreference mHeadsUpEnabled;
     private SystemSettingMasterSwitchPreference mGamingMode;
     private SecureSettingMasterSwitchPreference mPulse;
@@ -110,7 +114,65 @@ public class Misc extends SettingsPreferenceFragment implements
             getPreferenceScreen().removePreference(cutoutCategory);
         }
 
+        mSleepMode = findPreference("sleep_mode");
         updateMasterPrefs();
+        updateSleepModeSummary();
+    }
+
+    private void updateSleepModeSummary() {
+        if (mSleepMode == null) return;
+        boolean enabled = Settings.Secure.getIntForUser(getActivity().getContentResolver(),
+                Settings.Secure.SLEEP_MODE_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
+        int mode = Settings.Secure.getIntForUser(getActivity().getContentResolver(),
+                Settings.Secure.SLEEP_MODE_AUTO_MODE, 0, UserHandle.USER_CURRENT);
+        String timeValue = Settings.Secure.getStringForUser(getActivity().getContentResolver(),
+                Settings.Secure.SLEEP_MODE_AUTO_TIME, UserHandle.USER_CURRENT);
+        if (timeValue == null || timeValue.equals("")) timeValue = "20:00,07:00";
+        String[] time = timeValue.split(",", 0);
+        String outputFormat = DateFormat.is24HourFormat(getContext()) ? "HH:mm" : "h:mm a";
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern(outputFormat);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        LocalTime sinceValue = LocalTime.parse(time[0], formatter);
+        LocalTime tillValue = LocalTime.parse(time[1], formatter);
+        String detail;
+        switch (mode) {
+            default:
+            case 0:
+                detail = getActivity().getString(enabled
+                        ? R.string.night_display_summary_on_auto_mode_never
+                        : R.string.night_display_summary_off_auto_mode_never);
+                break;
+            case 1:
+                detail = getActivity().getString(enabled
+                        ? R.string.night_display_summary_on_auto_mode_twilight
+                        : R.string.night_display_summary_off_auto_mode_twilight);
+                break;
+            case 2:
+                if (enabled) {
+                    detail = getActivity().getString(R.string.night_display_summary_on_auto_mode_custom, tillValue.format(outputFormatter));
+                } else {
+                    detail = getActivity().getString(R.string.night_display_summary_off_auto_mode_custom, sinceValue.format(outputFormatter));
+                }
+                break;
+            case 3:
+                if (enabled) {
+                    detail = getActivity().getString(R.string.night_display_summary_on_auto_mode_custom, tillValue.format(outputFormatter));
+                } else {
+                    detail = getActivity().getString(R.string.night_display_summary_off_auto_mode_twilight);
+                }
+                break;
+            case 4:
+                if (enabled) {
+                    detail = getActivity().getString(R.string.night_display_summary_on_auto_mode_twilight);
+                } else {
+                    detail = getActivity().getString(R.string.night_display_summary_off_auto_mode_custom, sinceValue.format(outputFormatter));
+                }
+                break;
+        }
+        String summary = getActivity().getString(enabled
+                ? R.string.night_display_summary_on
+                : R.string.night_display_summary_off, detail);
+        mSleepMode.setSummary(summary);
     }
 
     private void updateMasterPrefs() {
@@ -203,12 +265,14 @@ public class Misc extends SettingsPreferenceFragment implements
     public void onResume() {
         super.onResume();
         updateMasterPrefs();
+        updateSleepModeSummary();
     }
 
     @Override
     public void onPause() {
         super.onPause();
         updateMasterPrefs();
+        updateSleepModeSummary();
     }
 
     @Override
