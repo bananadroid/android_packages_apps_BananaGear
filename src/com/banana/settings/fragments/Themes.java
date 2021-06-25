96538b529d25517fe413805da31525f69ca5f9ba
Rizky Benggolo <travarilo@gmail.com>
2021-06-25 17:52:16 +0700

    Gear: Reorganize themes section
    Change-Id: I4042c7a8356bdd50595d33b1252805380ae13298

--- src/com/banana/settings/fragments/Themes.java
+++ src/com/banana/settings/fragments/Themes.java
@@ -179,8 +179,6 @@ public class Themes extends DashboardFragment implements OnPreferenceChangeListe
         controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.adaptive_icon_shape"));
         controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.icon_pack.android"));
-        controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.signal_icon"));
         controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.wifi_icon"));
