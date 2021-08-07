b90c08ae3dd6036c6e537b1e3c55a75655a3a8ef
Rizky Benggolo <travarilo@gmail.com>
2021-08-07 19:37:56 +0700

    Gear: Move accent & gradient to own fragment
    Change-Id: I26c807a62a4db0aee4c8432a518353591ac6cd51

--- src/com/banana/settings/fragments/Themes.java
+++ src/com/banana/settings/fragments/Themes.java
@@ -42,8 +42,6 @@ import com.android.settingslib.search.SearchIndexable;
 import com.banana.settings.preferences.SwitchStylePreferenceController;
 import com.banana.settings.preferences.QsTileStylePreferenceController;
 import com.banana.settings.preferences.UiBlurPreferenceController;
-import com.banana.settings.preferences.RGBAccentPickerPreferenceController;
-import com.banana.settings.preferences.RGBGradientPickerPreferenceController;
 import com.banana.settings.preferences.Utils;
 
 import com.bananadroid.support.preferences.SecureSettingSwitchPreference;
@@ -137,8 +135,6 @@ public class Themes extends DashboardFragment implements OnPreferenceChangeListe
         controllers.add(mFontPickerPreference = new FontPickerPreferenceController(context, lifecycle));
         controllers.add(new QsTileStylePreferenceController(context));
         controllers.add(new UiBlurPreferenceController(context));
-        controllers.add(new RGBAccentPickerPreferenceController(context));
-        controllers.add(new RGBGradientPickerPreferenceController(context));
         return controllers;
     }
 
