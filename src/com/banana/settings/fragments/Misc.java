1dc95d2f5cef45443692f0170eec82b0e077a9c4
Pranav Vashi <neobuddy89@gmail.com>
2022-02-12 22:55:13 +0530

    Gear: User toggle for GamesProp [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I57756a791dcc8a24ca3770c7b59000bb9d265e46

--- src/com/banana/settings/fragments/Misc.java
+++ src/com/banana/settings/fragments/Misc.java
@@ -55,11 +55,15 @@ public class Misc extends SettingsPreferenceFragment implements
 
     private static final String POCKET_JUDGE = "pocket_judge";
     private static final String HEADS_UP_TIMEOUT_PREF = "heads_up_timeout";
+    private static final String KEY_GAMES_SPOOF = "use_games_spoof";
     private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
+
+    private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
     private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
 
     private Preference mPocketJudge;
     private CustomSeekBarPreference mHeadsUpTimeOut;
+    private SwitchPreference mGamesSpoof;
     private SwitchPreference mPhotosSpoof;
 
     @Override
@@ -81,6 +85,10 @@ public class Misc extends SettingsPreferenceFragment implements
         mHeadsUpTimeOut = (CustomSeekBarPreference)
                             prefScreen.findPreference(HEADS_UP_TIMEOUT_PREF);
         mHeadsUpTimeOut.setDefaultValue(getDefaultDecay(mContext));
+
+        mGamesSpoof = (SwitchPreference) prefScreen.findPreference(KEY_GAMES_SPOOF);
+        mGamesSpoof.setChecked(SystemProperties.getBoolean(SYS_GAMES_SPOOF, false));
+        mGamesSpoof.setOnPreferenceChangeListener(this);
 
         mPhotosSpoof = (SwitchPreference) prefScreen.findPreference(KEY_PHOTOS_SPOOF);
         mPhotosSpoof.setChecked(SystemProperties.getBoolean(SYS_PHOTOS_SPOOF, true));
@@ -115,13 +123,18 @@ public class Misc extends SettingsPreferenceFragment implements
                 Settings.System.RETICKER_STATUS, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.RETICKER_COLORED, 0, UserHandle.USER_CURRENT);
+        SystemProperties.set(SYS_GAMES_SPOOF, "false");
         SystemProperties.set(SYS_PHOTOS_SPOOF, "true");
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mPhotosSpoof) {
+        if (preference == mGamesSpoof) {
+            boolean value = (Boolean) newValue;
+            SystemProperties.set(SYS_GAMES_SPOOF, value ? "true" : "false");
+            return true;
+        } else if (preference == mPhotosSpoof) {
             boolean value = (Boolean) newValue;
             SystemProperties.set(SYS_PHOTOS_SPOOF, value ? "true" : "false");
             return true;
