0f79d6d6aad8347a8d0e95401a4ab17cc419cffa
00day0 <therandomuser11@gmail.com>
2022-04-17 11:32:15 +0530

    Gear: Introduce OOS style notification clear all button [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Icca98d0ed54e9bf94118d4f2084d9a2ac0dc351d

--- src/com/banana/settings/fragments/Notifications.java
+++ src/com/banana/settings/fragments/Notifications.java
@@ -91,6 +91,8 @@ public class Notifications extends DashboardFragment implements
                 Settings.System.NOTIFICATION_SOUND_VIB_SCREEN_ON, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.HEADS_UP_TIMEOUT, getDefaultDecay(mContext), UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.NOTIFICATION_MATERIAL_DISMISS, 0, UserHandle.USER_CURRENT);
     }
 
     @Override
