a66152c6a26f3a16364b327e52f4adc17b0fef8c
Rizky Benggolo <travarilo@gmail.com>
2022-07-29 23:33:28 +0700

    Gear: Move blur toggle under BananaGear [2/2]
    Change-Id: I27ef973d4a2025f85354f861e1e2f6ac889019c3

--- src/com/banana/settings/fragments/Themes.java
+++ src/com/banana/settings/fragments/Themes.java
@@ -20,12 +20,16 @@ import android.content.Context;
 import android.os.Bundle;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
+import androidx.fragment.app.Fragment;
 import androidx.preference.*;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.display.EnableBlursPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.search.Indexable;
 import com.android.settingslib.search.SearchIndexable;
 
@@ -60,6 +64,18 @@ public class Themes extends DashboardFragment implements
     @Override
     protected String getLogTag() {
         return TAG;
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, getSettingsLifecycle(), this);
+    }
+
+    private static List<AbstractPreferenceController> buildPreferenceControllers(
+            Context context, Lifecycle lifecycle, Fragment fragment) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new EnableBlursPreferenceController(context));
+        return controllers;
     }
 
     public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
