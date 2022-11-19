debee02f6d1825f5a3a0fbc7eb1246fa2b8a77b7
ShevT <evgeny.shishkov@gmail.com>
2022-11-19 09:39:48 +0300

    Gear: Allow toggling netflix spoofing [2/2]
    * neobuddy: Update prop for netflix spoof
    
    Change-Id: If5794ab873df2dab67febab5d3cafa4d4fb37cb1

--- src/com/banana/settings/fragments/Misc.java
+++ src/com/banana/settings/fragments/Misc.java
@@ -36,6 +36,7 @@ import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.development.SystemPropPoker;
 import com.android.settingslib.search.Indexable;
 import com.android.settingslib.search.SearchIndexable;
 
@@ -57,6 +58,7 @@ public class Misc extends SettingsPreferenceFragment implements
 
     private static final String KEY_GAMES_SPOOF = "use_games_spoof";
     private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
+    private static final String KEY_NETFLIX_SPOOF = "use_netflix_spoof";
 
     private static final String KEY_FORCE_FULL_SCREEN = "display_cutout_force_fullscreen_settings";
     private static final String SMART_PIXELS = "smart_pixels";
@@ -64,9 +66,11 @@ public class Misc extends SettingsPreferenceFragment implements
 
     private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
     private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
+    private static final String SYS_NETFLIX_SPOOF = "persist.sys.pixelprops.netflix";
 
     private SwitchPreference mGamesSpoof;
     private SwitchPreference mPhotosSpoof;
+    private SwitchPreference mNetFlixSpoof;
     private Preference mShowCutoutForce;
     private Preference mSmartPixels;
     private CustomSeekBarPreference mHeadsUpTimeOut;
@@ -87,6 +91,10 @@ public class Misc extends SettingsPreferenceFragment implements
         mPhotosSpoof = (SwitchPreference) prefScreen.findPreference(KEY_PHOTOS_SPOOF);
         mPhotosSpoof.setChecked(SystemProperties.getBoolean(SYS_PHOTOS_SPOOF, true));
         mPhotosSpoof.setOnPreferenceChangeListener(this);
+
+        mNetFlixSpoof = (SwitchPreference) findPreference(KEY_NETFLIX_SPOOF);
+        mNetFlixSpoof.setChecked(SystemProperties.getBoolean(SYS_NETFLIX_SPOOF, false));
+        mNetFlixSpoof.setOnPreferenceChangeListener(this);
 
 	final String displayCutout =
             mContext.getResources().getString(com.android.internal.R.string.config_mainBuiltInDisplayCutout);
@@ -124,6 +132,7 @@ public class Misc extends SettingsPreferenceFragment implements
         ContentResolver resolver = mContext.getContentResolver();
         SystemProperties.set(SYS_GAMES_SPOOF, "false");
         SystemProperties.set(SYS_PHOTOS_SPOOF, "true");
+        SystemProperties.set(SYS_NETFLIX_SPOOF, "false");
         Settings.System.putIntForUser(resolver,
                 Settings.System.NOTIFICATION_GUTS_KILL_APP_BUTTON, 0, UserHandle.USER_CURRENT);
         SmartPixels.reset(mContext);
@@ -150,10 +159,17 @@ public class Misc extends SettingsPreferenceFragment implements
         if (preference == mGamesSpoof) {
             boolean value = (Boolean) newValue;
             SystemProperties.set(SYS_GAMES_SPOOF, value ? "true" : "false");
+            SystemPropPoker.getInstance().poke();
             return true;
         } else if (preference == mPhotosSpoof) {
             boolean value = (Boolean) newValue;
             SystemProperties.set(SYS_PHOTOS_SPOOF, value ? "true" : "false");
+            SystemPropPoker.getInstance().poke();
+            return true;
+        } else if (preference == mNetFlixSpoof) {
+            boolean value = (Boolean) newValue;
+            SystemProperties.set(SYS_NETFLIX_SPOOF, value ? "true" : "false");
+            SystemPropPoker.getInstance().poke();
             return true;
         }
         return false;
