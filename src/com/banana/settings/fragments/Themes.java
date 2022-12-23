e7bcc5f061f03fe612582daf21a1b9e82b64dd97
Rizky Benggolo <travarilo@gmail.com>
2022-12-23 16:30:42 +0700

    Gear: Add toggle for about phone header style [2/3]
    Change-Id: Ic862674da82a08b94028b8aaa74e2f758c210056

--- src/com/banana/settings/fragments/Themes.java
+++ src/com/banana/settings/fragments/Themes.java
@@ -38,6 +38,7 @@ import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.search.SearchIndexable;
 
+import com.banana.settings.preferences.AboutHeaderPreferenceController;
 import com.banana.settings.preferences.CustomUIPreferenceController;
 
 import java.util.ArrayList;
@@ -81,6 +82,7 @@ public class Themes extends DashboardFragment implements OnPreferenceChangeListe
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
         controllers.add(new EnableBlursPreferenceController(context));
         controllers.add(new CustomUIPreferenceController(context));
+        controllers.add(new AboutHeaderPreferenceController(context));
         return controllers;
     }
 
