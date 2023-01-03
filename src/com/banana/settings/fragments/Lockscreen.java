d82acb8096044a2e520468ad4ccefd5c31dac884
minaripenguin <minaripenguin@users.noreply.github.com>
2023-01-03 11:12:04 +0800

    Gear: Configurable lockscreen clock color [2/2]
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Change-Id: Idd4893abcbcd2b68180f95a127a73835ee362639

--- src/com/banana/settings/fragments/Lockscreen.java
+++ src/com/banana/settings/fragments/Lockscreen.java
@@ -48,11 +48,13 @@ public class Lockscreen extends SettingsPreferenceFragment implements
     private static final String KEY_FP_SUCCESS_VIBRATE = "fp_success_vibrate";
     private static final String KEY_FP_ERROR_VIBRATE = "fp_error_vibrate";
     private static final String KEY_UDFPS_SETTINGS = "udfps_settings";
+    private static final String KG_CUSTOM_CLOCK_COLOR_ENABLED = "kg_custom_clock_color_enabled";
 
     private Preference mRippleEffect;
     private Preference mFingerprintVib;
     private Preference mFingerprintVibErr;
     private Preference mUdfpsSettings;
+    private SwitchPreference mKGCustomClockColor;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -82,10 +84,22 @@ public class Lockscreen extends SettingsPreferenceFragment implements
                 gestCategory.removePreference(mFingerprintVibErr);
             }
         }
+
+mKGCustomClockColor = (SwitchPreference) findPreference(KG_CUSTOM_CLOCK_COLOR_ENABLED);
+        boolean mKGCustomClockColorEnabled = Settings.Secure.getIntForUser(resolver,
+                Settings.Secure.KG_CUSTOM_CLOCK_COLOR_ENABLED, 0, UserHandle.USER_CURRENT) != 0;
+        mKGCustomClockColor.setChecked(mKGCustomClockColorEnabled);
+        mKGCustomClockColor.setOnPreferenceChangeListener(this);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mKGCustomClockColor) {
+            boolean val = (Boolean) newValue;
+            Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.KG_CUSTOM_CLOCK_COLOR_ENABLED, val ? 1 : 0, UserHandle.USER_CURRENT);
+            return true;
+        }
         return false;
     }
 
