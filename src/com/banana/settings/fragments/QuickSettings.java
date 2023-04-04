18150ad93feb56d0b0ad074de1c7d2ebb149d502
Rizky Benggolo <travarilo@gmail.com>
2023-04-04 11:26:26 +0700

    Revert "Gear: Allow to disable qs footer warnings [2/2]"
    This reverts commit fa5572ec8168be867a3d5dc79dd67c3def20ef5d.
    
    Change-Id: I1e876b5a7db893fe4469a393511815fbb4739489

--- src/com/banana/settings/fragments/QuickSettings.java
+++ src/com/banana/settings/fragments/QuickSettings.java
@@ -144,8 +144,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         Settings.System.putIntForUser(resolver,
                 Settings.System.SHOW_QS_CLOCK, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
-                Settings.System.QS_FOOTER_WARNINGS, 1, UserHandle.USER_CURRENT);
-        Settings.System.putIntForUser(resolver,
                 Settings.System.SHOW_QS_DATE, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.QS_FOOTER_DATA_USAGE, 0, UserHandle.USER_CURRENT);
