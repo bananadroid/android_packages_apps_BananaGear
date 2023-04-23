1f0ed08d032c851a7903d9212059706803a931f0
minaripenguin <minaripenguin@users.noreply.github.com>
2023-04-23 22:47:19 +0800

    Gear: Spoof remaining apps toggle [2/2]
    And also cleanup code
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Rizky Benggolo <travarilo@gmail.com>
    Change-Id: Iccffca3ae51942bde176d3de1b679c0066d19d5d

--- src/com/banana/settings/fragments/Misc.java
+++ src/com/banana/settings/fragments/Misc.java
@@ -62,21 +62,10 @@ public class Misc extends DashboardFragment implements
 
     public static final String TAG = "Misc";
 
-    private static final String KEY_GAMES_SPOOF = "use_games_spoof";
-    private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
-    private static final String KEY_NETFLIX_SPOOF = "use_netflix_spoof";
-
     private static final String KEY_FORCE_FULL_SCREEN = "display_cutout_force_fullscreen_settings";
     private static final String POCKET_JUDGE = "pocket_judge";
     private static final String SMART_PIXELS = "smart_pixels";
 
-    private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
-    private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
-    private static final String SYS_NETFLIX_SPOOF = "persist.sys.pixelprops.netflix";
-
-    private SwitchPreference mGamesSpoof;
-    private SwitchPreference mPhotosSpoof;
-    private SwitchPreference mNetFlixSpoof;
     private Preference mPocketJudge;
     private Preference mShowCutoutForce;
     private Preference mSmartPixels;
@@ -94,18 +83,6 @@ public class Misc extends DashboardFragment implements
         final PreferenceScreen prefScreen = getPreferenceScreen();
         final Context mContext = getActivity().getApplicationContext();
         final Resources res = getResources();
-
-        mGamesSpoof = (SwitchPreference) prefScreen.findPreference(KEY_GAMES_SPOOF);
-        mGamesSpoof.setChecked(SystemProperties.getBoolean(SYS_GAMES_SPOOF, false));
-        mGamesSpoof.setOnPreferenceChangeListener(this);
-
-        mPhotosSpoof = (SwitchPreference) prefScreen.findPreference(KEY_PHOTOS_SPOOF);
-        mPhotosSpoof.setChecked(SystemProperties.getBoolean(SYS_PHOTOS_SPOOF, true));
-        mPhotosSpoof.setOnPreferenceChangeListener(this);
-
-        mNetFlixSpoof = (SwitchPreference) findPreference(KEY_NETFLIX_SPOOF);
-        mNetFlixSpoof.setChecked(SystemProperties.getBoolean(SYS_NETFLIX_SPOOF, false));
-        mNetFlixSpoof.setOnPreferenceChangeListener(this);
 
 	final String displayCutout =
             mContext.getResources().getString(com.android.internal.R.string.config_mainBuiltInDisplayCutout);
@@ -130,9 +107,6 @@ public class Misc extends DashboardFragment implements
 
     public static void reset(Context mContext) {
         ContentResolver resolver = mContext.getContentResolver();
-        SystemProperties.set(SYS_GAMES_SPOOF, "false");
-        SystemProperties.set(SYS_PHOTOS_SPOOF, "true");
-        SystemProperties.set(SYS_NETFLIX_SPOOF, "false");
         SmartPixels.reset(mContext);
         Settings.System.putIntForUser(resolver,
                 Settings.System.CHARGING_ANIMATION, 1, UserHandle.USER_CURRENT);
@@ -144,22 +118,6 @@ public class Misc extends DashboardFragment implements
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mGamesSpoof) {
-            boolean value = (Boolean) newValue;
-            SystemProperties.set(SYS_GAMES_SPOOF, value ? "true" : "false");
-            SystemPropPoker.getInstance().poke();
-            return true;
-        } else if (preference == mPhotosSpoof) {
-            boolean value = (Boolean) newValue;
-            SystemProperties.set(SYS_PHOTOS_SPOOF, value ? "true" : "false");
-            SystemPropPoker.getInstance().poke();
-            return true;
-        } else if (preference == mNetFlixSpoof) {
-            boolean value = (Boolean) newValue;
-            SystemProperties.set(SYS_NETFLIX_SPOOF, value ? "true" : "false");
-            SystemPropPoker.getInstance().poke();
-            return true;
-        }
         return false;
     }
 
