b45c58aaa3e2fb123e471d6640e55f0a0a1ffd53
darkobas <darkobas@gmail.com>
2023-04-23 03:03:35 +0000

    Gear: add option to enable AOD on charging only [2/2]

--- src/com/banana/settings/fragments/DozeSettings.java
+++ src/com/banana/settings/fragments/DozeSettings.java
@@ -266,7 +266,8 @@ public class DozeSettings extends SettingsPreferenceFragment implements
                 Settings.Secure.DOZE_GESTURE_VIBRATE, 0, UserHandle.USER_CURRENT);
         Settings.Secure.putIntForUser(resolver,
                 Settings.Secure.PULSE_ON_NEW_TRACKS, 0, UserHandle.USER_CURRENT);
-
+        Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.DOZE_ON_CHARGE, 0, UserHandle.USER_CURRENT);
     }
 
     @Override
