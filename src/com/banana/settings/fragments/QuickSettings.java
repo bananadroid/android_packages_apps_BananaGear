0ff1a166f77484be52555db6bd3d3f93dc228a88
Rizky Benggolo <travarilo@gmail.com>
2024-02-28 13:17:14 +0000

    Gear: Remove QS tile and panel style customization
    Revert "Revert "Gear: Make QS tile style and QS panel style exclusive""
    
    This reverts commit 39126b6352946a163c935d37141913dded579d16.
    
    Revert "Gear: Move out qs tiles settings to their own fragment"
    
    This reverts commit 69c08f3743985c474bbca9ddf024fe5e36b7432d.
    
    Revert "Gear: Make QS tile style and QS panel style exclusive"
    
    This reverts commit 72cb8b733f3ffe80cb2030d9302e0df514aa028a.
    
    Revert "Gear: Add QS Panel Style [3/3]"
    
    This reverts commit a813b1f6b071bd90263185a498981f6ff23f789e.
    
    Revert "Gear: A11 QS UI Style [3/3]"
    
    This reverts commit 34155b727ed564d0ce5378e2901a0bc03869ecb9.
    
    Revert "Gear: Allow setting quick QS rows [2/2]"
    
    This reverts commit 582bcaefcbe58341a5fc71c4c4ae5d3f6974b5a2.
    
    Revert "Gear: Allow to change QS tile label text size [2/2]"
    
    This reverts commit 0f04e842a05d52660b6f15aa9713d7670a27ee32.
    
    Revert "Gear: Add QS tile layout settings [2/2]"
    
    This reverts commit cf0b46572a7de2416b5e2408ed16fe1046f1e8aa.

--- src/com/banana/settings/fragments/QuickSettings.java
+++ src/com/banana/settings/fragments/QuickSettings.java
@@ -25,12 +25,13 @@ import android.provider.Settings;
 import androidx.preference.*;
 
 import com.android.internal.logging.nano.MetricsProto;
-
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.Indexable;
 import com.android.settingslib.search.SearchIndexable;
+
+import com.banana.support.preferences.CustomSeekBarPreference;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,11 +46,17 @@ public class QuickSettings extends DashboardFragment implements
     private static final String KEY_BRIGHTNESS_SLIDER_POSITION = "qs_brightness_slider_position";
     private static final String KEY_BRIGHTNESS_SLIDER_HAPTIC = "qs_brightness_slider_haptic";
     private static final String KEY_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
+    private static final String KEY_PREF_TILE_ANIM_STYLE = "qs_tile_animation_style";
+    private static final String KEY_PREF_TILE_ANIM_DURATION = "qs_tile_animation_duration";
+    private static final String KEY_PREF_TILE_ANIM_INTERPOLATOR = "qs_tile_animation_interpolator";
 
     private ListPreference mShowBrightnessSlider;
     private ListPreference mBrightnessSliderPosition;
     private SwitchPreference mBrightnessSliderHaptic;
     private SwitchPreference mShowAutoBrightness;
+    private ListPreference mTileAnimationStyle;
+    private CustomSeekBarPreference mTileAnimationDuration;
+    private ListPreference mTileAnimationInterpolator;
 
     @Override
     protected int getPreferenceScreenResId() {
@@ -83,17 +90,29 @@ public class QuickSettings extends DashboardFragment implements
         } else {
             prefScreen.removePreference(mShowAutoBrightness);
         }
+
+        mTileAnimationStyle = (ListPreference) findPreference(KEY_PREF_TILE_ANIM_STYLE);
+        mTileAnimationDuration = (CustomSeekBarPreference) findPreference(KEY_PREF_TILE_ANIM_DURATION);
+        mTileAnimationInterpolator = (ListPreference) findPreference(KEY_PREF_TILE_ANIM_INTERPOLATOR);
+
+        mTileAnimationStyle.setOnPreferenceChangeListener(this);
+
+        int tileAnimationStyle = Settings.System.getIntForUser(resolver,
+                Settings.System.QS_TILE_ANIMATION_STYLE, 0, UserHandle.USER_CURRENT);
+        updateAnimTileStyle(tileAnimationStyle);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        ContentResolver resolver = getActivity().getContentResolver();
-
         if (preference == mShowBrightnessSlider) {
             int value = Integer.parseInt((String) newValue);
             mBrightnessSliderPosition.setEnabled(value > 0);
             mBrightnessSliderHaptic.setEnabled(value > 0);
             if (mShowAutoBrightness != null)
                 mShowAutoBrightness.setEnabled(value > 0);
+            return true;
+        } else if (preference == mTileAnimationStyle) {
+            int value = Integer.parseInt((String) newValue);
+            updateAnimTileStyle(value);
             return true;
         }
         return false;
@@ -112,9 +131,20 @@ public class QuickSettings extends DashboardFragment implements
         Settings.System.putIntForUser(resolver,
                 Settings.System.QS_TRANSPARENCY, 100, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
+                Settings.System.QS_TILE_ANIMATION_STYLE, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.QS_TILE_ANIMATION_DURATION, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.QS_TILE_ANIMATION_INTERPOLATOR, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
                 Settings.System.QS_BRIGHTNESS_SLIDER_HAPTIC, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.QS_DUAL_TONE, 1, UserHandle.USER_CURRENT);
+    }
+
+    private void updateAnimTileStyle(int tileAnimationStyle) {
+        mTileAnimationDuration.setEnabled(tileAnimationStyle != 0);
+        mTileAnimationInterpolator.setEnabled(tileAnimationStyle != 0);
     }
 
     @Override
